656f21aff8f525173c30ae6e255ea422a2d0312e
Herrera Daniel <danielherrera2009@hotmail.es>
2023-08-28 17:39:34 -0300

    web MemoTest fix(onclick)

--- docs/12_web_memoTest/index.js
+++ docs/12_web_memoTest/index.js
@@ -1,172 +1,157 @@
-// FALTA ARREGLAR que cuando se le pregunte al usuario "Desea volver a jugar"
-//  y responda NO entonces los casilleros no se puedan clickear
+// Se recibe la etiqueta <script> del html
+const script = document.querySelector('script[src="index.js"]');
+// Creamos array con url's de imagenes traidas como atributo de la etiqueta <script>
+const imagenes = JSON.parse(script.getAttribute('data'));
+console.log(imagenes);
 
-document.addEventListener("DOMContentLoaded", function() {
+let imagenesVolteadas = [];
+let paresEncontrados = 0;
+let intentos = 6;
 
-    // Se recibe la etiqueta <script> del html
-    const script = document.querySelector('script[src="index.js"]');
-    // Creamos array con url's de imagenes traidas como atributo de la etiqueta <script>
-    const imagenes = JSON.parse(script.getAttribute('data'));
-    console.log(imagenes);
+const gridItems = document.querySelectorAll(".grid-item");
+const intentosDisplay = document.querySelector(".intentos");
 
-    let imagenesVolteadas = [];
-    let paresEncontrados = 0;
-    let intentos = 6;
+function clickear(gridItem) {
+    console.log("click "+gridItem)
+    // Del casillero se recibe la imagen y se le asigna 'src' concatenando su 'alt'
+    let img = gridItem.querySelector("img");
+    img.setAttribute("src",`img/pic_${img.alt}.jpg`);
 
-    const gridItems = document.querySelectorAll(".grid-item");
-    const intentosDisplay = document.querySelector(".intentos");
+    // Si hay menos de 2 casilleros volteados Y el casillero actual NO esta volteado
+    if (imagenesVolteadas.length < 2 && !gridItem.classList.contains("volteada")) {
+        // Se le asigna al casillero la clase 'volteada'
+        gridItem.classList.add("volteada");
+    
+        // Nos llevamos el 'alt' de la imagen del casillero para guardarlo en un array
+        let imgIndex = img.alt;
+        imagenesVolteadas.push(imgIndex);
 
-    // Se recorren los casilleros
-    gridItems.forEach(function(gridItem) {
-        // A cada casillero se le asigna un evento click
-        gridItem.addEventListener("click", function clickear() {
-
-            // Del casillero se recibe la imagen y se le asigna 'src' concatenando su 'alt'
-            let img = gridItem.querySelector("img");
-            img.setAttribute("src",`img/pic_${img.alt}.jpg`);
-
-            // Si hay menos de 2 casilleros volteados Y el casillero actual NO esta volteado
-            if (imagenesVolteadas.length < 2 && !gridItem.classList.contains("volteada")) {
-                // Se le asigna al casillero la clase 'volteada'
-                gridItem.classList.add("volteada");
-            
-                // Nos llevamos el 'alt' de la imagen del casillero para guardarlo en un array
-                let imgIndex = img.alt;
-                imagenesVolteadas.push(imgIndex);
-
-                // Si ya hay 2 casilleros volteados
-                if (imagenesVolteadas.length === 2) {
-                    verificarPar();
-                }
-            }
-        });
-    });
-
-    function verificarPar() {
-        let img1 = imagenesVolteadas[0];
-        let img2 = imagenesVolteadas[1];
-
-        if (img1 === img2) {
-            imagenesVolteadas = [];
-            paresEncontrados++;
-
-            if (paresEncontrados === imagenes.length / 2) {
-                setTimeout(function() {
-                    Swal.fire({
-                        title: "¡Has ganado!",
-                        text: "¿Quieres jugar de nuevo?",
-                        icon: "success",
-                        showCancelButton: true,
-                        confirmButtonText: "Sí",
-                        cancelButtonText: "No"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            resetGame();
-                        }else{
-                            gridItems.forEach(function(gridItem) {
-                                // Remover el evento click de cada casillero
-                                gridItem.removeEventListener("click", clickear);
-                            });
-                        }
-                    });
-                }, 500);
-            }
-        } else {
-
-            imagenesVolteadas = [];
-            intentos--;
-            
-            gridItems.forEach(function(gridItem) {
-                const gridImg = gridItem.querySelector("img");
-                if (gridImg){
-                    let url = gridImg.alt;  
-
-                    if (url === img1 || url === img2){
-                        setTimeout(function() {
-                            gridImg.setAttribute("src", 'img/pic_0.jpg');
-                            gridItem.classList.remove("volteada");
-                        }, 1100);
-
-                    }
-                }
-
-            });
-
-            if (intentos === 0) {
-                setTimeout(function() {
-                    Swal.fire({
-                        title: "¡Has perdido!",
-                        text: "¿Quieres jugar de nuevo?",
-                        icon: "error",
-                        showCancelButton: true,
-                        confirmButtonText: "Sí",
-                        cancelButtonText: "No"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            resetGame();
-                        }else{
-                            gridItems.forEach(function(gridItem) {
-                                // Remover el evento click de cada casillero
-                                gridItem.removeEventListener("click", clickear);
-                            });
-                        }
-                    });
-                }, 500);
-            }
-        }
-
-        // Para que intentos no baje de 0
-        if(intentos<0){
-            intentosDisplay.textContent = `Intentos restantes: 0`;
-        }else{
-            intentosDisplay.textContent = `Intentos restantes: ${intentos}`;
+        // Si ya hay 2 casilleros volteados
+        if (imagenesVolteadas.length === 2) {
+            verificarPar();
         }
     }
+}
 
-    function resetGame() {
-        gridItems.forEach(function(gridItem) {
-            gridItem.classList.remove("volteada");
-        });
+function verificarPar() {
+    let img1 = imagenesVolteadas[0];
+    let img2 = imagenesVolteadas[1];
+
+    if (img1 === img2) {
+        imagenesVolteadas = [];
+        paresEncontrados++;
+
+        if (paresEncontrados === imagenes.length / 2) {
+            setTimeout(function() {
+                Swal.fire({
+                    title: "¡Has ganado!",
+                    text: "¿Quieres jugar de nuevo?",
+                    icon: "success",
+                    showCancelButton: true,
+                    confirmButtonText: "Sí",
+                    cancelButtonText: "No"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        resetGame();
+                    }else{
+                        gridItems.forEach(function(gridItem) {
+                            // Remover el evento click de cada casillero
+                            gridItem.removeAttribute("onclick");
+                        });
+                    }
+                });
+            }, 500);
+        }
+    } else {
 
         imagenesVolteadas = [];
-        paresEncontrados = 0;
-        intentos = 6;
-        intentosDisplay.textContent = `Intentos restantes: ${intentos}`;
-        mezclarImagenes();
-    }
-
-    // Funcion que retorna los numeros de un string, acorde para los url de las imagenes
-    function numeroDeImagen(url) {
-        let num = "";
-        for(let i=0; i<url.length; i++){
-            let caracter = url.substring(i,i+1);
-            if(!isNaN(parseInt(caracter))){
-                num += caracter;
-            }
-        }
-        return num;
-    }
-
-    // Funcion que mezcla y asigna las imagenes a los casilleros
-    function mezclarImagenes() {
-        for (let i = imagenes.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [imagenes[i], imagenes[j]] = [imagenes[j], imagenes[i]];
-        }
-
-        // Se usa un indice auxiliar para ir recorriendo imagen por imagen
-        let indice = 0;
-
+        intentos--;
+        
         gridItems.forEach(function(gridItem) {
-            const img = gridItem.querySelector("img");
-            // Se le asigna 'alt' a cada imagen con el numero de su url (ej: img/pic_10.jpg => 10)
-            img.setAttribute("alt", `${numeroDeImagen(imagenes[indice])}`);
-            indice++;
-            // Se le asigna 'src' con un url de una imagen incognito, luego se usará 'alt' para revelar su verdadera imagen
-            img.setAttribute("src", "img/pic_0.jpg");
+            const gridImg = gridItem.querySelector("img");
+            if (gridImg){
+                let url = gridImg.alt;  
+
+                if (url === img1 || url === img2){
+                    setTimeout(function() {
+                        gridImg.setAttribute("src", 'img/pic_0.jpg');
+                        gridItem.classList.remove("volteada");
+                    }, 500);
+
+                }
+            }
+
         });
+
+        if (intentos === 0) {
+            setTimeout(function() {
+                Swal.fire({
+                    title: "¡Has perdido!",
+                    text: "¿Quieres jugar de nuevo?",
+                    icon: "error",
+                    showCancelButton: true,
+                    confirmButtonText: "Sí",
+                    cancelButtonText: "No"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        resetGame();
+                    }else{
+                        gridItems.forEach(function(gridItem) {
+                            // Remover el evento click de cada casillero
+                            gridItem.removeAttribute("onclick");
+                        });
+                    }
+                });
+            }, 500);
+        }
     }
 
-    // Se inicia el javascript
+    intentosDisplay.textContent = `Intentos restantes: ${intentos}`;
+}
+
+function resetGame() {
+    gridItems.forEach(function(gridItem) {
+        gridItem.classList.remove("volteada");
+    });
+
+    imagenesVolteadas = [];
+    paresEncontrados = 0;
+    intentos = 6;
+    intentosDisplay.textContent = `Intentos restantes: ${intentos}`;
     mezclarImagenes();
-});
+}
+
+// Funcion que retorna los numeros de un string, acorde para los url de las imagenes
+function numeroDeImagen(url) {
+    let num = "";
+    for(let i=0; i<url.length; i++){
+        let caracter = url.substring(i,i+1);
+        if(!isNaN(parseInt(caracter))){
+            num += caracter;
+        }
+    }
+    return num;
+}
+
+// Funcion que mezcla y asigna las imagenes a los casilleros
+function mezclarImagenes() {
+    for (let i = imagenes.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [imagenes[i], imagenes[j]] = [imagenes[j], imagenes[i]];
+    }
+
+    // Se usa un indice auxiliar para ir recorriendo imagen por imagen
+    let indice = 0;
+
+    gridItems.forEach(function(gridItem) {
+        const img = gridItem.querySelector("img");
+        // Se le asigna 'alt' a cada imagen con el numero de su url (ej: img/pic_10.jpg => 10)
+        img.setAttribute("alt", `${numeroDeImagen(imagenes[indice])}`);
+        indice++;
+        // Se le asigna 'src' con un url de una imagen incognito, luego se usará 'alt' para revelar su verdadera imagen
+        img.setAttribute("src", "img/pic_0.jpg");
+    });
+}
+
+// Se inicia el javascript
+mezclarImagenes();
